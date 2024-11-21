e7996733f47bb1722f8dba2571af4fa100851c82
Francesc Constans <123585528+fconstanslasalle@users.noreply.github.com>
2024-11-21 15:09:23 +0100

    Versió 1

--- esp_32_php_to_led/esp_32_php_to_led.ino
+++ esp_32_php_to_led/esp_32_php_to_led.ino
@@ -5,7 +5,12 @@
 
 #define WIFI_SSID "LSG-CFGS" 
 #define WIFI_PASSWORD "CFGS-L@S@lleGr@ci@"
+#define NUM_LEDS 64
+#define DATA_PIN 2
+#define LED_TYPE WS2812B
+#define COLOR_ORDER GRB
 
+CRGB leds[NUM_LEDS];
 WebServer server(80);
 
 void handleRoot() {
@@ -20,33 +25,47 @@ void handleRoot() {
   // Convertir la cadena en un array de enteros
   int valores[NUM_LEDS];
   int indice = 0;
+  int numUnos = 0;
+  int numCeros = 0;
   for (int i = 0; i < cadena_array.length(); i++) {
     if (cadena_array.charAt(i) == ',') {
       indice++;
-    } else {
-      valores[indice] = valores[indice] * 10 + (cadena_array.charAt(i) - '0');
+    } 
+    if (cadena_array.charAt(i)=='1'){
+      valores[indice]=1;
+      indice++;
+      numUnos++;
+    }
+    if (cadena_array.charAt(i)=='0'){
+      valores[indice]=0;
+      indice++;
+      numCeros++;
     }
   }
 
   // Actualizar la matriz de LEDs con los valores del array
   for (int i = 0; i < NUM_LEDS; i++) {
-    if (valores[i]!=0){
-      leds[i]=CRGB::White;
-    }
-    else{
-      leds[i]=CRGB::Black
-    }
+    if (valores[i]==1){
+      leds[i]=CRGB::White;   
+      Serial.println("Encendiendo led número"+1);
+    }else{
+      leds[i]=CRGB::Black;
+      Serial.println("Apagando led número"+i);
+    }  
   }
   FastLED.show();
+ // Enviar una respuesta al cliente
+ server.send(200, "text/plain", "Array actualizado correctamente");
 
-  // Enviar una respuesta al cliente
-  server.send(200, "text/plain", "Array recibido correctamente");
 }
 
 void setup() {
   delay(1000); 
   Serial.begin(115200);
   Serial.println("Iniciando sistema...");
+
+  FastLED.addLeds<LED_TYPE, DATA_PIN, COLOR_ORDER>(leds, NUM_LEDS);
+  FastLED.setBrightness(50); // Ajusta el brillo según sea necesario
 
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
   while (WiFi.status() != WL_CONNECTED) {
